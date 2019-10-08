761d6c13dad2da582cc34658552d0609458e2224
vdelendik <vadim.delendik@gmail.com>
2019-10-08 13:00:10 +0300

    updated linux provider start

--- bin/start-linux-provider.sh
+++ bin/start-linux-provider.sh
@@ -9,16 +9,8 @@ PRIVATE_IP=192.168.88.95
 export RETHINKDB_PORT_28015_TCP="tcp://${PRIVATE_IP}:28015"
 
 
-# DEVICE NAME    | TYPE      | VERSION| UDID                                     |APPIUM|  WDA  | MJPEG | IWDP  | STF_MIN | STF_MAX | DEVICE IP
-# iPhone_7       | phone     | 12.3.1 | 4828ca6492816ddd4996fea31c175f7ab97cbc19 | 4841 | 20001 | 20002 | 20003 |  7701   |  7710   | 192.168.88.14
-
-nohup node /Users/build/tools/stf/lib/cli ios-device --serial 4828ca6492816ddd4996fea31c175f7ab97cbc19 \
-        --provider iMac-Developer.local --screen-port 7701 --connect-port 20002 --vnc-port 7732 --public-ip ${PUBLIC_IP} --group-timeout 3600 \
-        --storage-url https://${PUBLIC_IP}/ --adb-host 127.0.0.1 --adb-port 5037 --screen-jpeg-quality 40 --screen-ping-interval 30000 \
-        --screen-ws-url-pattern ws://${publicIp}:${publicPort} --connect-url-pattern ${publicIp}:${publicPort} --heartbeat-interval 10000 \
-        --boot-complete-timeout 60000 --vnc-initial-size 600x800 --mute-master never \
-        --connect-app-dealer tcp://${PRIVATE_IP}:7160 --connect-dev-dealer tcp://${PRIVATE_IP}:7260 \
-        --wda-host 192.168.88.14 \
-        --wda-port 20001 --udid-storage false --iproxy false --connect-sub tcp://${PRIVATE_IP}:7250 --connect-push tcp://${PRIVATE_IP}:7270 --no-cleanup &
-
-
+nohup node /app/bin/stf provider --name MYNAME --min-port=7701 --max-port=7710 \
+  --connect-sub tcp://${PRIVATE_IP}:7250 --connect-push tcp://${PRIVATE_IP}:7270 \
+  --group-timeout 3600 --public-ip ${PUBLIC_IP} --storage-url https://${PUBLIC_IP}/ \
+  --screen-jpeg-quality 40 --heartbeat-interval 10000 --vnc-initial-size 600x800 --vnc-port 5900 \
+  --no-cleanup --screen-ws-url-pattern wss://${PUBLIC_IP}/d/MYNAME/<%= serial %>/<%= publicPort %>/ &
