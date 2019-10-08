5aee715399003c5b89e29964b2671e24037e0814
vdelendik <vadim.delendik@gmail.com>
2019-10-08 13:03:01 +0300

    minor fixes

--- bin/start-linux-provider.sh
+++ bin/start-linux-provider.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Example of the ios-device statr script
+# Example of the android-device stf provider start script
 
 PUBLIC_IP=stage.qaprosoft.com
 PRIVATE_IP=192.168.88.95
@@ -9,7 +9,7 @@ PRIVATE_IP=192.168.88.95
 export RETHINKDB_PORT_28015_TCP="tcp://${PRIVATE_IP}:28015"
 
 
-nohup node /app/bin/stf provider --name MYNAME --min-port=7701 --max-port=7710 \
+nohup node stf provider --name MYNAME --min-port=7701 --max-port=7710 \
   --connect-sub tcp://${PRIVATE_IP}:7250 --connect-push tcp://${PRIVATE_IP}:7270 \
   --group-timeout 3600 --public-ip ${PUBLIC_IP} --storage-url https://${PUBLIC_IP}/ \
   --screen-jpeg-quality 40 --heartbeat-interval 10000 --vnc-initial-size 600x800 --vnc-port 5900 \
