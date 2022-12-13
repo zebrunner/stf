00dcb4cdd03aaf0df9552d36cdd330aa89a7a3f1
vdelendik <vdelendik@solvd.com>
2022-12-13 04:04:26 +0300

    #539 and #304: moved device settings to advanced and removed automation tab

--- res/app/control-panes/automation/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = angular.module('stf.automation', [
-  require('./device-settings').name,
-])
-  .run(['$templateCache', function($templateCache) {
-    $templateCache.put(
-      'control-panes/automation/automation.pug'
-      , require('./automation.pug')
-    )
-  }])
