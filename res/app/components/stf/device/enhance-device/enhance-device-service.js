a915d2f8438bf447fd2daa8e910e2bd214b56ed6
vdelendik <vdelendik@solvd.com>
2022-12-14 15:09:46 +0300

    #341: override default icons by platform for iOS and Android platforms

--- res/app/components/stf/device/enhance-device/enhance-device-service.js
+++ res/app/components/stf/device/enhance-device/enhance-device-service.js
@@ -48,8 +48,8 @@ module.exports = function EnhanceDeviceServiceFactory($filter, AppState) {
   function enhanceDevice(device) {
     device.enhancedName = device.name || device.model || device.serial || 'Unknown'
     device.enhancedModel = device.model || 'Unknown'
-    device.enhancedImage120 = '/static/app/devices/icon/x120/' + (device.image || '_default.jpg')
-    device.enhancedImage24 = '/static/app/devices/icon/x24/' + (device.image || '_default.jpg')
+    device.enhancedImage120 = '/static/app/devices/icon/x120/' + (device.platform || device.image || '_default.jpg')
+    device.enhancedImage24 = '/static/app/devices/icon/x24/' + (device.platform || device.image || '_default.jpg')
     device.enhancedStateAction = $filter('statusNameAction')(device.state)
     device.enhancedStatePassive = $filter('statusNamePassive')(device.state)
   }
