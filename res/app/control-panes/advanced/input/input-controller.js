e05dcce5509c67ef9373f86b680da200513e0c6d
solvd <solvd@MacBook-Pro-de-solvd.local>
2024-04-15 14:57:35 -0300

    #938 Temporary disabling mute button after use

--- res/app/control-panes/advanced/input/input-controller.js
+++ res/app/control-panes/advanced/input/input-controller.js
@@ -1,5 +1,22 @@
 module.exports = function InputCtrl($scope) {
 
+  let muteButton = document.getElementById('muteButton')
+
+  muteButton.onclick = function() {
+    if (muteButton.getAttribute('disabled')) {
+      return
+    }
+
+    $scope.control.keyPress('mute')
+    muteButton.setAttribute('disabled', 'disabled')
+    muteButton.setAttribute('style', 'cursor: wait;')    
+
+    setTimeout(function() {
+      muteButton.removeAttribute('disabled')
+      muteButton.setAttribute('style', 'cursor: pointer;')
+    }, 16000)
+  } 
+
   $scope.press = function(key) {
     $scope.control.keyPress(key)
   }
