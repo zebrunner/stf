af04949e299eae469b626cbd6c7a122ba46b63f9
Ignacio Narvaja <85379690+ignacionar@users.noreply.github.com>
2024-09-26 10:35:25 -0300

    Use device.name in enhance-device-service.js

--- res/app/components/stf/device/enhance-device/enhance-device-service.js
+++ res/app/components/stf/device/enhance-device/enhance-device-service.js
@@ -46,7 +46,7 @@ module.exports = function EnhanceDeviceServiceFactory($filter, AppState) {
   }
 
   function enhanceDevice(device) {
-    device.enhancedName = device.model || device.product  || device.marketName || device.serial || 'Unknown'
+    device.enhancedName = device.name || device.model || device.product  || device.marketName || device.serial || 'Unknown'
     device.enhancedModel = device.model || 'Unknown'
     device.enhancedImage120 = '/static/app/devices/icon/x120/' + (device.platform || device.image || '_default.jpg')
     device.enhancedImage24 = '/static/app/devices/icon/x24/' + (device.platform || device.image || '_default.jpg')
